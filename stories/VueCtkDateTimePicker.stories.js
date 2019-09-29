08cea1fa09048d01b1ea7228a485e0a84b1f8916
chriswongatcuhk <wc38.wong@gmail.com>
2019-09-29 10:07:46 +0800

    Add yourValue

--- stories/VueCtkDateTimePicker.stories.js
+++ stories/VueCtkDateTimePicker.stories.js
@@ -1,19 +1,25 @@
-import { linkTo } from '@storybook/addon-links';
-import Vue from 'vue';
-import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
-import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+import { linkTo } from '@storybook/addon-links'
+import Vue from 'vue'
+import VueCtkDateTimePicker from 'vue-ctk-date-time-picker'
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css'
 
-Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker);
+Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker)
 
 export default {
   title: 'VueCtkDateTimePicker'
 };
 
+const gg = '123'
 export const toStorybook = () => ({
+  data() {
+    return {
+      yourValue: null,
+    }
+  },
   components: { VueCtkDateTimePicker },
-  template: '<VueCtkDateTimePicker v-model="yourValue" />',
-});
+  template: `<VueCtkDateTimePicker v-model="yourValue" />`,
+})
 
 toStorybook.story = {
   name: 'to Storybook',
-};
+}
