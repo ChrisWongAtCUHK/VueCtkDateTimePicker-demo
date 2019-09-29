be171ce372415ea27306d45f6a02a9dac684d608
chriswongatcuhk <wc38.wong@gmail.com>
2019-09-29 09:33:47 +0800

    Install storybook

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueCtkDateTimePicker from 'vue-ctk-date-time-picker'
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css'
+
+Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker)
 
 Vue.config.productionTip = false
 
